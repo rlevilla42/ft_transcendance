dbea68bff18bf353996cfbafa720fd36456ec319
rlevilla42 <rlevilla42@gmail.com>
2025-05-04 19:25:31 +0200

    jwt ok et docker backend reparé et relancant tout

---
+++ backend/src/main/java/com/backend/service/UserService.java
@@ -0,0 +1,42 @@
+package com.backend.service;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.backend.dto.LoginDto;
+import com.backend.dto.RegisterDto;
+import com.backend.entity.User;
+import com.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(RegisterDto registerDto) {
+        if (userRepository.findByUsername(registerDto.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exist");
+        }
+        User user = User.builder()
+        .password(passwordEncoder.encode(registerDto.getPassword()))
+        .username(registerDto.getUsername()).build();
+        return userRepository.save(user);
+    };
+
+    public User login(LoginDto loginDto) {
+        Optional<User> optionalUser = userRepository.findByUsername(loginDto.getUsername());
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("Invalid username");
+        }
+        User user = optionalUser.get();
+        if (!passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        return user;
+    };
+}
